e169eca724920a6d70d9be291a380fbce717f810
ldnup <44421144+ldn005402@users.noreply.github.com>
2021-08-11 10:28:20 -0700

    Create 954. Array of Doubled Pairs.py

---
+++ 954. Array of Doubled Pairs.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canReorderDoubled(self, A):
+        cnt = collections.Counter(A)
+        for a in sorted(A, key = abs):
+            if cnt[a] and cnt[a * 2]:
+                cnt[a] -= 1
+                cnt[a * 2] -= 1  
+        return all(cnt[a] == 0 for a in A)
