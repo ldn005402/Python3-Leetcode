3015c06b1cc0035c0c1092e065dbdc7ec3f791f0
ldnup <44421144+ldn005402@users.noreply.github.com>
2021-08-16 13:23:39 -0700

    Create 265. Paint House II.py

---
+++ 265. Paint House II.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def minCostII(self, costs):
+        """
+        :type costs: List[List[int]]
+        :rtype: int
+        """
+        if not costs:
+            return 0
+        for i in range(1,len(costs)):
+            for k in range(len(costs[0])):
+                costs[i][k] += min(costs[i-1][:k]+costs[i-1][k+1:])
+        return min(costs[-1])
