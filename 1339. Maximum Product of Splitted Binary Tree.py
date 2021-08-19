525ee48487c3ce585241320e0d96e87c41a7f8c9
ldnup <44421144+ldn005402@users.noreply.github.com>
2021-08-19 10:10:51 -0700

    Create 1339. Maximum Product of Splitted Binary Tree.py

---
+++ 1339. Maximum Product of Splitted Binary Tree.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProduct(self, root):
+        self.res = total = 0
+
+        def s(root):
+            if not root: return 0
+            left, right = s(root.left), s(root.right)
+            self.res = max(self.res, left * (total - left), right * (total - right))
+            return left + right + root.val
+
+        total = s(root)
+        s(root)
+        return self.res % (10**9 + 7)
