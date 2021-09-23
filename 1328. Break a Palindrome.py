8f89b2d97eb04eacc3419a838290e43ab1e80ef4
ldnup <44421144+ldn005402@users.noreply.github.com>
2021-09-23 13:40:09 -0700

    Create 1328. Break a Palindrome.py

---
+++ 1328. Break a Palindrome.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def breakPalindrome(self, palindrome):
+        """
+        :type palindrome: str
+        :rtype: str
+        """ 
+        p1 = 0
+        p2 = len(palindrome)-1
+        new_pal = list(palindrome)
+        while p1 < p2:
+            if new_pal[p1] != "a":
+                new_pal[p1] = "a"
+                return "".join(new_pal)
+            p1+=1
+            p2-=1
+        if len(new_pal) == 1: return ""
+        new_pal[-1] = "b"
+        return "".join(new_pal)
