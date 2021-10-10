2c1abd86230e00facd7d4e5b34b7654c5ae83ad3
ldnup <44421144+ldn005402@users.noreply.github.com>
2021-10-10 00:18:23 -0700

    Create 201. Bitwise AND of Numbers Range.py

---
+++ 201. Bitwise AND of Numbers Range.py
@@ -0,0 +1,15 @@
+import math
+class Solution:
+    def rangeBitwiseAnd(self, m: int, n: int) -> int:
+        if m == 0:
+            return 0
+        top = int(math.log(n,2))
+        bottom = int(math.log(m,2))
+        if top != bottom:
+            return 0
+
+        res = m
+        for i in range(m + 1, n + 1):
+            res = res & i
+            
+        return res
