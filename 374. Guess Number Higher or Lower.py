ccb6eb0965d94519d51df19fe761fd330b7d1349
ldnup <44421144+ldn005402@users.noreply.github.com>
2021-10-12 00:06:57 -0700

    Create 374. Guess Number Higher or Lower.py

---
+++ 374. Guess Number Higher or Lower.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        low = 1
+        high = n
+        while low <= high:
+            mid = (low + high)//2
+            res =  guess(mid)
+            if res == 0 :
+                return mid
+            elif res == -1:
+                high = mid - 1
+            else:
+                low = mid + 1
