ed0860dbd98c86e1373394905f49914597a9a447
ldnup <44421144+ldn005402@users.noreply.github.com>
2021-11-17 10:18:56 -0800

    Create 62. Unique Paths.py

---
+++ 62. Unique Paths.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        table = [[0]*n for _ in range(m)]
+        for i in range(n):
+            table[0][i] = 1
+        for j in range(m):
+            table[j][0] = 1
+        for i in range(1,m):
+            for j in range(1,n):
+                table[i][j] = table[i-1][j] + table[i][j-1]
+        return table[m-1][n-1]
