d0e88c157b5a7bfd67216d109f0192b6a76ee039
ldnup <44421144+ldn005402@users.noreply.github.com>
2021-11-23 10:33:21 -0800

    Create 136. Single Number.py

---
+++ 136. Single Number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+    	L, d = len(nums), {}
+    	for n in nums:
+    		if n in d: del d[n]
+    		else: d[n] = 1
+    	return list(d)[0]
