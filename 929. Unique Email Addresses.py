b5daf0416613bc14e2d661d3a24a0eb6dcfe1e5a
ldnup <44421144+ldn005402@users.noreply.github.com>
2021-12-19 01:46:29 -0800

    Create 929. Unique Email Addresses.py

---
+++ 929. Unique Email Addresses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numUniqueEmails(self, emails):
+        """
+        :type emails: List[str]
+        :rtype: int
+        """
+        email_set = set()
+        for email in emails:
+            local_name,domain_name = email.split("@")
+            local_name ="".join(local_name.split('+')[0].split('.'))
+            email = local_name +'@' + domain_name
+            email_set.add(email)
+        return len(email_set)
